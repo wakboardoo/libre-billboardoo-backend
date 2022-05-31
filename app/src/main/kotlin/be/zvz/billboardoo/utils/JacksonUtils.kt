4122725a55516ee363462ab4a2caca0eaa703f0c
JellyBrick <shlee1503@naver.com>
2022-06-01 05:00:59 +0900

    fix kotlin module

--- app/src/main/kotlin/be/zvz/billboardoo/utils/JacksonUtils.kt
+++ app/src/main/kotlin/be/zvz/billboardoo/utils/JacksonUtils.kt
@@ -2,8 +2,9 @@ package be.zvz.billboardoo.utils
 
 import com.fasterxml.jackson.module.blackbird.BlackbirdModule
 import com.fasterxml.jackson.module.kotlin.jsonMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
 object JacksonUtils {
     val blackbirdModule = BlackbirdModule()
-    val mapper = jsonMapper().registerModule(blackbirdModule)
+    val mapper = jsonMapper().registerKotlinModule().registerModule(blackbirdModule)
 }
