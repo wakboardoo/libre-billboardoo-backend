6c977720714340b80016c1b7994832eb8b40edbf
JellyBrick <shlee1503@naver.com>
2022-05-31 23:57:52 +0900

    fix: crashing (plugin RateLimit is not installed)

--- app/src/main/kotlin/be/zvz/billboardoo/plugins/Routing.kt
+++ app/src/main/kotlin/be/zvz/billboardoo/plugins/Routing.kt
@@ -2,10 +2,13 @@ package be.zvz.billboardoo.plugins
 
 import be.zvz.billboardoo.routes.rankRouting
 import be.zvz.billboardoo.routes.songRouting
+import guru.zoroark.ratelimit.RateLimit
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    install(RateLimit)
+
     routing {
         rankRouting()
         songRouting()
